d5e7202330ff02557d8e3d2998fd0cc354a4d648
Ikkatos23 <100568977@student.swin.edu.au>
2019-08-12 08:10:03 +1000

    Final

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,10 +9,32 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)
-        {
-            Message myMessage;
-            myMessage = new Message("Hello World - from Message Object");
+        {  
+            Message myMessage = new Message("Hello World - from Message Object");
             myMessage.Print();
+            Message[] messages = new Message[4];
+            messages[0] = new Message("Chris is a good teacher");
+            messages[1] = new Message("Hey Stew");
+            messages[2] = new Message("Bap Boop Beep");
+            messages[3] = new Message("That is a silly name");
+            Console.WriteLine("Please enter your name!");
+            string name = Console.ReadLine().ToLower();
+            if (name == "chris")
+            {
+                messages[0].Print();
+            }
+            else if (name == "kyle")
+            {
+                messages[1].Print();
+            }
+            else if (name == "charlie")
+            {
+                messages[2].Print();
+            }
+            else
+            {
+                messages[3].Print();
+            }
             Console.ReadLine();
         }
     }
